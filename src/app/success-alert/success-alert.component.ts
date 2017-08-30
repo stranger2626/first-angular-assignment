40dfb58c2b8d844f62cf8fcace39aaffbad368b2
Matsvei Murashka <stranger2626@gmail.com>
2017-08-31 00:56:28 +0300

    initial commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.successAllert',
+  template: '<h1>Success Message</h1>\n',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: ['h1 {color: red}']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
